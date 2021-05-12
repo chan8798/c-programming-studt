2fa33ac2c2dc87f39351c147560429bb131febf7
KIM CHAN JONG <80242038+chan8798@users.noreply.github.com>
2021-05-12 22:04:03 +0900

    Add files via upload

---
+++ add1.c.cpp
@@ -0,0 +1,17 @@
+﻿/*두 개의 숫자의 합을 계산하는 프로그램*/
+#include <stdio.h>
+
+int main(void)
+{
+	int x; //첫번째 정수를 저장할 변수
+	int y; //두번째 정수를 저장할 변수
+	int sum;//두 정수의 합을 저장하는 변수
+
+	x = 100;
+	y = 200;
+
+	sum = x + y;
+	printf("두수의 합: %d", sum);
+
+	return 0;
+}
